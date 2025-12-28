4324374942f6b51aef46ab2dd5f5032cccbacbb4
katu <abugameone@gmail.com>
2025-12-28 22:24:38 +0800

    menambah function eksekusi1()

---
+++ src/model/executor.py
@@ -0,0 +1,35 @@
+from src.config.db_config import mypool
+from mysql-connector import Error
+
+def eksekusi1():
+    try:
+        with mypool.get_connection() ass conn:
+            return (conn.user, conn.server_host)
+    except Error as db_error:
+        print(f"db_error: {db_error}")
+    except Exception as e:
+        print(f"error: {e}")
+
+def eksekusi2():
+    pass
+
+def eksekusi3():
+    pass
+
+def eksekusi4():
+    pass
+
+def eksekusi5():
+    pass
+
+daftar = {
+    "eksekusi1": eksekusi1,
+    "eksekusi2": eksekusi2,
+    "eksekusi3": eksekusi3,
+    "eksekusi4": eksekusi4,
+    "eksekusi5": eksekusi5,
+    }
+
+def eksekusi(pilih):
+    fungsi = daftar.get(pilih)
+    return fungsi()
