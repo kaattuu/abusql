856066b55ce7d09f394a69f9282c834a1ef9c684
katu <abugameone@gmail.com>
2025-12-29 01:13:01 +0800

    persiapan riset display tabel

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-from src.control.control1 import start
+# from src.control.control1 import start
 
-start()
+# start()
+
+from src.views.table import tabel
+
+data1 = ("no", "nama", "alamat")
+
+tabel("tabel1", data1)
