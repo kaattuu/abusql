7ac29ed7d34ae701e0c2784fb278b32f468e7527
katu <abugameone@gmail.com>
2025-12-29 18:32:47 +0800

    memperbaharui function tabel1(data)

--- src/views/table.py
+++ src/views/table.py
@@ -1,5 +1,20 @@
-def tabel1(data):
+def tabel0(data):
     print(data)
+
+def tabel1(data):
+    jumlah_elemen = len(data)
+    jumlah_huruf  = [len(i) for i in data]
+    padding       = 2
+    garis_atas    = f"┌{"─" * (max(jumlah_huruf) + padding)}┐"
+    garis_bawah   = f"└{"─" * (max(jumlah_huruf) + padding)}┘"
+    print()
+    print(garis_atas)
+    hasil = ""
+    for i in data:
+        hasil += f"│ {i} │"
+    print(hasil)
+    print(garis_bawah)
+    print()
 
 def tabel2(data):
     pass
@@ -14,6 +29,7 @@ def tabel5(data):
     pass
 
 daftar = {
+    "tabel0": tabel0,
     "tabel1": tabel1,
     "tabel2": tabel2,
     "tabel3": tabel3,
