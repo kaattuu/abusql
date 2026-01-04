1306f47e6ef5158b718d5b75edcd73127698f3e1
katu <abugameone@gmail.com>
2026-01-04 20:07:13 +0800

    feat(menu-tabel): implement of function to select database

--- src/views/pilih1.py
+++ src/views/pilih1.py
@@ -56,7 +56,7 @@ def pilih_tabel():
     while True:
         pilihan = input("")
         match pilihan:
-            case "1"  : print(f"kamu memilih nomor '{pilihan}'. terimakasih!")
+            case "1"  : kendali("kendali3", "config2", "c3")
             case "2"  : print(f"kamu memilih nomor '{pilihan}'. terimakasih!")
             case "3"  : print(f"kamu memilih nomor '{pilihan}'. terimakasih!")
             case "4"  : print(f"kamu memilih nomor '{pilihan}'. terimakasih!")
